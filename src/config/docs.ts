cea7208829725825ddfac7566b998a3aeb7aa635
Juan-M-Panozzo-Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2024-03-28 10:24:10 -0300

    Eliminar páginas y ejemplos no implementados en la documentación

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -44,65 +44,65 @@ export const docsConfig: DocsConfig = {
         },
       ],
     },
-    {
-      title: "Pages",
-      items: [
-        {
-          title: "Landing",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Changelog",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Wait List",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-    {
-      title: "Examples",
-      items: [
-        // {
-        //   title: "Introduction",
-        //   href: "/docs/in-progress",
-        //   disabled: true,
-        // },
-        {
-          title: "Static Blog",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Docs & Guides",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Blog with Astro DB",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Ecommerce",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Animes (GraphQL)",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Authentification (Lucia)",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
+    // {
+    //   title: "Pages",
+    //   items: [
+    //     {
+    //       title: "Landing",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Changelog",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Wait List",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Examples",
+    //   items: [
+    //     // {
+    //     //   title: "Introduction",
+    //     //   href: "/docs/in-progress",
+    //     //   disabled: true,
+    //     // },
+    //     {
+    //       title: "Static Blog",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Docs & Guides",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Blog with Astro DB",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Ecommerce",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Animes (GraphQL)",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Authentification (Lucia)",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //   ],
+    // },
   ],
 };
