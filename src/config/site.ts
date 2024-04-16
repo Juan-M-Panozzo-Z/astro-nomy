ef333a4b0d0246edcf10bd7fad38153e4a218e99
Juan-M-Panozzo-Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2024-04-16 11:49:28 -0300

    Actualizar imagen del logo en el pie de página

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,15 +1,15 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "Mindbridge",
-  description:
-    "Landingpage del proyecto mindbridge - una app para conectar a pacientes con profesionales de la salud mental",
-  url: "https://mindbridge.com.ar",
-  ogImage: "/og.jpeg",
-  links: {
-    twitter: "https://x.com/mindbridge_app",
-    instagram: "https://www.instagram.com/mindbridge.arg/",
-    github: "https://github.com/Juan-M-Panozzo-Z",
-    linkedin: "https://www.linkedin.com/in/juan-manuel-panozzo-zenere/",
-  },
+    name: "Mindbridge",
+    description:
+        "Bienvenido a Mindbridge - una app para conectar a pacientes con profesionales de la salud mental",
+    url: "https://mindbridge.com.ar",
+    ogImage: "/og.jpeg",
+    links: {
+        twitter: "https://x.com/mindbridge_app",
+        instagram: "https://www.instagram.com/mindbridge.arg/",
+        github: "https://github.com/Juan-M-Panozzo-Z",
+        linkedin: "https://www.linkedin.com/in/juan-manuel-panozzo-zenere/",
+    },
 };
